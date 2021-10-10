14383aeef2df9c1a2ceb1ceab60efa3feff9b6b9
DonBass <ladasfeed@mail.ru>
2021-10-10 17:36:46 +0300

    install packages

--- parser/index.ts
+++ parser/index.ts
@@ -2,6 +2,20 @@
 const folderPath = process.cwd().replace(/[\\]/g, '/')
 const swaggerRaw = require(folderPath+'/swagger.json')
 const {initMethod} = require('./methods/init')
+const { exec } = require("child_process");
 const swagger = swaggerRaw
 
 initMethod(swagger)
+
+console.log('Success')
+exec(`cd ${folderPath}/dist && npm install express cors classnames --save-dev`,  (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})

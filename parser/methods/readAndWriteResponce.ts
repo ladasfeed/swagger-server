f9beee330d21ab2f3f0ac88113c72f0c7090b222
DonBass <ladasfeed@mail.ru>
2022-02-08 15:20:22 +0300

    one of support

--- parser/methods/readAndWriteResponce.ts
+++ parser/methods/readAndWriteResponce.ts
@@ -4,27 +4,24 @@ const swagger = require(`${folderPath}/swagger.json`)
 const deepSearch = (obj: any) => {
     getProp(obj);
 
-    function getProp(o:any) {
+    function getProp(o: any) {
         for (let prop in o) {
-            if (typeof(o[prop]) === 'object') {
+            if (typeof (o[prop]) === 'object') {
                 getProp(o[prop]);
             } else {
-                console.log('Finite value: '+o[prop])
+                console.log('Finite value: ' + o[prop])
             }
         }
     }
 }
 
 export const readAndWriteData = (ref: string | Object) => {
-    let newObj:any = {}
-    let props:any = ref
-
-
+    let newObj: any = {}
+    let props: any = ref
 
     if (typeof ref == "string") {
         const pathArray = ref.substr(2).split('/')
         const field = swagger.components.schemas[pathArray[2]]
-        console.log(field)
         if (field.hasOwnProperty('type') && ['boolean', 'string', 'integer'].includes(field.type)) {
             props = field
         } else {
@@ -44,28 +41,19 @@ export const readAndWriteData = (ref: string | Object) => {
     for (let key in props) {
         if (props[key].hasOwnProperty('example')) {
             newObj[key] = props[key].example
-        }
-        else if (props[key].hasOwnProperty('type') && props[key].type == 'array') {
+        } else if (props[key].hasOwnProperty('type') && props[key].type == 'array') {
             if (props[key].items) {
                 newObj[key] = [readAndWriteData(props[key].items)]
             }
-        }
-        else if (props[key].hasOwnProperty('type') && props[key].type == 'object' && props[key].hasOwnProperty('properties')) {
+        } else if (props[key].hasOwnProperty('type') && props[key].type == 'object' && props[key].hasOwnProperty('properties')) {
             if (props[key].properties) {
                 newObj[key] = readAndWriteData(props[key].properties)
             }
-        }
-        else if (key == '$ref') {
-            if (props[key]) {
-                newObj = readAndWriteData(props[key])
-            }
-        }
-        else if (typeof props[key] == 'object') {
-            if (props[key]) {
-                newObj[key] = readAndWriteData(props[key])
-            }
-        }
-        else {
+        } else if (key == '$ref') {
+            newObj = readAndWriteData(props[key])
+        } else if (typeof props[key] == 'object') {
+            newObj[key] = readAndWriteData(props[key])
+        } else {
             newObj[key] = props[key]
         }
     }
